73efab558b6a0049eafb6dd3551be3b58159ba9d
arka1089 <159404091+arka1089@users.noreply.github.com>
2024-09-21 09:09:12 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+app.get("/",(req,res)=>{
+    res.sendFile(__dirname+"/index.html");
+})
+app.post("/Login",(req,res)=>{
+    console.log(req.body);
+    res.send("true");
+})
+
+app.listen(5000,()=>{
+    console.log("server started at port 5000");
+})
